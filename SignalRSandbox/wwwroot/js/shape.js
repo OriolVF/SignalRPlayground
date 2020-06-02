94e07b7f45c9768801737a083b657c6265ce3a41
Oriol <oriol@funda.nl>
2020-06-02 16:25:06 +0200

    Initial commit

---
+++ SignalRSandbox/wwwroot/js/shape.js
@@ -0,0 +1,24 @@
+﻿$shape = $("#shape");
+var connectionShape = new signalR.HubConnectionBuilder()
+    .withUrl('/shapeHub')
+    .build();
+
+var initialOffset;
+
+$(document).ready(function () {
+    initialOffset = $shape.offset();
+    console.log('offset: ', initialOffset);
+});
+
+connectionShape.on('shapeMoved', function (x, y) {
+    $shape.css({ left: x, top: y });
+    console.log('received offsets: ', x, y);
+});
+
+connectionShape.start().then(
+    $shape.draggable({
+        drag: function () {
+            connectionShape.invoke("MoveShape", parseInt(this.offsetLeft - initialOffset.left), parseInt(this.offsetTop - initialOffset.top));
+        }
+    })
+);
